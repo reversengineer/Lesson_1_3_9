72b36d4ea467f8bfd18cd8ae35500b889214ab0f
Tyler Collie <709621@hayscisd.net>
2019-10-18 13:31:03 -0500

    Commiting to Master :)
    Lol

--- team2.py
+++ team2.py
@@ -1,14 +1,14 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
+#     team_name: 
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Tyler' # Only 10 chars displayed.
+strategy_name = 'Commit to Master'
+strategy_description = "It doesn't decide, the master does."
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.

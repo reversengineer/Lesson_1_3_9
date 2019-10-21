ab1477a1dac650adbb0ed44eefd8a0daa3e55cdc
Marco Deleon <592811@hayscisd.net>
2019-10-21 14:10:03 -0500

    Update team3.py

--- team3.py
+++ team3.py
@@ -6,16 +6,18 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'The Unstoppable' # Only 10 chars displayed.
+strategy_name = 'Betray'
+strategy_description = 'If they have done wrong'
     
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    their_betray_count + 0
+    return 'c'
     
-    Make my move.
-    Returns 'c' or 'b'. 
+    
+    
+    
+    
     '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.

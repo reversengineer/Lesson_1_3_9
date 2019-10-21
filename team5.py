1885d8653e83d539b76897686d5e3d19efdc3d32
Evan Gonzales <593735@hayscisd.net>
2019-10-21 13:58:13 -0500

    Update team5.py

--- team5.py
+++ team5.py
@@ -1,16 +1,9 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
 
 team_name = 'team5' # Only 10 chars displayed.
-strategy_name = 'Collude first 75 rounds unless betrayed. Betray round 76 forward.'
+strategy_name = 'Collude first 50 rounds unless betrayed. Betray round 76 forward.'
 strategy_description = '''\
 Betray if ever betrayed.
-If I haven't been betrayed yet, I'll betray starting with the 75th round.
+If I haven't been betrayed yet, I'll betray starting with the 51st round.
 '''
 import random 
     
@@ -21,19 +14,10 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    if 'b' in their_history or len(their_history) > 100:
+    if 'b' in their_history or len(their_history) >= 50:
         return 'b'
     else:
         return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
@@ -53,22 +37,17 @@ def test_move(my_history, their_history, my_score, their_score, result):
 
 if __name__ == '__main__':
      
-    # Test 1: Betray on first move.
+    
     if test_move(my_history='',
               their_history='', 
               my_score=0,
               their_score=0,
               result='b'):
          print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
+    
     test_move(my_history='bbb',
               their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
+             
               my_score=0, 
               their_score=0,
               result='b')             
